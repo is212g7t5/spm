eda8c8448a3c4791912958a8cf68957a8d8df7fd
branfats <92558018+branfats@users.noreply.github.com>
2022-09-18 22:01:31 +0800

    Alembic 0002 + skill model
    Created Alembic Revision ID 0002 to include skill table.
    Updated skill model accordingly from previous PR

--- backend/app/models/role.py
+++ backend/app/models/role.py
@@ -6,4 +6,3 @@ from app.db.base_class import Base
 class Role(Base):
     role_id = Column(Integer, primary_key=True, index=True)
     role_name = Column(String, nullable=False)
-    linked_skill = Column(Integer, ForeignKey("skill.skill_id")) 
--- backend/app/models/skill.py
+++ backend/app/models/skill.py
@@ -5,4 +5,7 @@ from app.db.base_class import Base
 
 class Skill(Base):
     skill_id = Column(Integer, primary_key=True, index=True)
+    course_id = Column(String, ForeignKey("course.course_id"), nullable=False) 
     skill_name = Column(String, nullable=False)
+    skill_desc = Column(String)
+    Is_Active = Column(Boolean)
