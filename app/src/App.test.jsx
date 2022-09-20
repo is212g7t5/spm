09caae02f7098eeddd7a5d2cbddedcb9fcc7900e
charmjj <jjlim.2019@sis.smu.edu.sg>
2022-09-20 14:34:58 +0800

    init rolespage and routing

--- app/src/App.test.jsx
+++ app/src/App.test.jsx
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Navbar from './components/Navbar';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('navbar', () => {
+  render(<Navbar />);
+  const title = screen.getByText(/LJPS/i);
+  expect(title).toBeInTheDocument();
+})
