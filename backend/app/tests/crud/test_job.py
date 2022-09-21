057f616b5ab6b2d474822b88b2ed72836e619855
charmjj <jjlim.2019@sis.smu.edu.sg>
2022-09-22 00:51:14 +0800

    fix broken unit tests for job

--- backend/app/tests/crud/test_job.py
+++ backend/app/tests/crud/test_job.py
@@ -9,7 +9,7 @@ from app.tests.utils.utils import random_lower_string
 def test_create_job(db: Session) -> None:
     job_name = random_lower_string(50)
     job_desc = random_lower_string(255)
-    job_in = JobCreate(job_name=job_name, job_desc=job_desc)
+    job_in = JobCreate(job_name=job_name, job_desc=job_desc, is_active=True)
     job = crud.job.create(db=db, obj_in=job_in,)
     assert job.job_name == job_name
     assert job.job_desc == job_desc
@@ -26,7 +26,7 @@ def test_update_job(db: Session) -> None:
     job = create_random_job(db)
     new_job_name = random_lower_string(20)
     assert new_job_name != job.job_name
-    job_update = JobUpdate(job_name=new_job_name)
+    job_update = JobUpdate(job_name=new_job_name, job_desc=job.job_desc, is_active=job.is_active)
     job2 = crud.job.update(db=db, db_obj=job, obj_in=job_update)
     assert job2.job_id == job.job_id
     assert job2.job_desc == job.job_desc
