f8f9450b6bccdc932b6f58d2fb293b0596a3a9d6
charmjj <jjlim.2019@sis.smu.edu.sg>
2022-09-23 00:27:01 +0800

    update package.json and package-lock.json, remove unused import in App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Courses from './routes/courses';
 import Jobs from './routes/jobs';
