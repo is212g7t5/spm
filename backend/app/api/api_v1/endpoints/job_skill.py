9efdc08eafced79eda0c1c1e6e0c49e0ac803a6d
deadline2359 <89028059+deadline2359@users.noreply.github.com>
2022-09-25 19:22:41 +0800

    resolved nits

--- backend/app/api/api_v1/endpoints/job_skill.py
+++ backend/app/api/api_v1/endpoints/job_skill.py
@@ -82,30 +82,6 @@ def create_job_skill(
     return job_skill
 
 
-# @router.put("/{job_id}&{skill_id}", response_model=schemas.JobSkill)
-# def update_job_skill_by_id(
-#     *,
-#     db: Session = Depends(deps.get_db),
-#     job_id: int,
-#     skill_id: int,
-# ) -> Any:
-#     """
-#     Update a combination of job and skill.
-#     """
-#     job_skill = crud.job_skill.get(db, job_id=job_id, skill_id=skill_id)
-#     if not job_skill:
-#         raise HTTPException(
-#             status_code=404,
-#             detail="Combination of Job and Skill not found",
-#         )
-#     job_skill_in = schemas.JobSkillCreate(
-#         skill_id=job_skill.skill_id,
-#         job_id=job_id or job_skill.job_id,
-#     )
-#     job_skill = crud.job_skill.update(db, db_obj=job_skill, obj_in=job_skill_in)
-#     return job_skill
-
-
 @router.delete("/{job_id}&{skill_id}", response_model=schemas.JobSkill)
 def delete_job_skill_by_id(
     *,
