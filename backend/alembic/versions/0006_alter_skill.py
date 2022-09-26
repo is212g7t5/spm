d79b6e1152af00b916fcd0f7ff0eccc9fffe0e04
branfats <92558018+branfats@users.noreply.github.com>
2022-09-27 01:16:18 +0800

    alembic 0006 alter skill

--- backend/alembic/versions/0006_alter_skill.py
+++ backend/alembic/versions/0006_alter_skill.py
@@ -1,15 +1,15 @@
 """Init database
 
-Revision ID: 0004
-Revises: 0003
-Create Date: 2022-09-24 00:00:00
+Revision ID: 0006
+Revises: 0005
+Create Date: 2022-09-25 00:00:00
 
 """
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "0004"
-down_revision = "0003"
+revision = "0006"
+down_revision = "0005"
 branch_labels = None
 depends_on = None
 
@@ -17,7 +17,6 @@ depends_on = None
 UPGRADE_SQL = """
 ALTER TABLE IF NOT EXISTS `skill`
 DROP COLUMN `Course_ID`;
-
 """
 
 DOWNGRADE_SQL = """
