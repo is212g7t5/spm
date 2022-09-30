7f450f29e46110a1775f52a46a152e8aa9df25d1
theianchia <theianchia@users.noreply.github.com>
2022-09-30 09:40:57 +0800

    Updated with flowbite js files and learning journey tile

--- app/src/components/learningJourney/LearningJourneyTile.jsx
+++ app/src/components/learningJourney/LearningJourneyTile.jsx
@@ -7,20 +7,17 @@ function LearningJourneyTile({ learningId, jobName, jobDesc, isJobActive }) {
   }
 
   return (
-    <div className="max-w-sm bg-white rounded-lg border border-gray-100 hover:bg-gray-100 hover:shadow-lg hover:shadow-cyan-200 mt-10">
+    <div className="max-w-sm bg-white rounded-lg border border-gray-100 hover:shadow-lg hover:shadow-orange-200 mt-10 mx-5">
       <div className="flex justify-end px-4 pt-4">
-        <button id="dropdownButton" data-dropdown-toggle="dropdown" className="inline-block text-gray-500 hover:bg-white rounded-lg text-sm p-1.5" type="button">
+        <button id="dropdownButton" data-dropdown-toggle="dropdown" className="inline-block text-gray-500 hover:bg-gray-100 focus:ring-2 focus:ring-gray-100 rounded-lg text-sm p-1.5" type="button">
           <span className="sr-only">Open dropdown</span>
           <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"/></svg>
         </button>
 
-        <div id="dropdown" className="hidden z-10 w-44 text-base list-none bg-white rounded divide-y divide-gray-100">
+        <div id="dropdown" className="hidden z-10 w-44 text-base list-none bg-white border border-gray-100 rounded divide-y divide-gray-100">
           <ul className="py-1" aria-labelledby="dropdownButton">
           <li>
             <a href="/#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Edit</a>
-          </li>
-          <li>
-            <a href="/#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Export Data</a>
           </li>
           <li>
             <a href="/#" className="block py-2 px-4 text-sm text-red-600 hover:bg-gray-100">Delete</a>
