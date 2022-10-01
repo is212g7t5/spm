10411db0110d0c1f242e5748008eed0df1f96f41
charmjj <jjlim.2019@sis.smu.edu.sg>
2022-10-02 00:07:48 +0800

    use prop in lj details

--- app/src/components/learningJourney/learningJourneyDetails/index.jsx
+++ app/src/components/learningJourney/learningJourneyDetails/index.jsx
@@ -5,7 +5,7 @@ import JobAndSkillsContainer from "./JobAndSkillsContainer";
 function LearningJourneyDetails( { ljId }) {
     return (
         <>
-            <h1 className="my-2 lg:text-2xl font-bold lg:text-center">Learning Journey 3</h1>
+            <h1 className="my-2 lg:text-2xl font-bold lg:text-center">Learning Journey {ljId}</h1>
             <div className="grid grid-cols-4">
                 <CourseContainer />
                 <JobAndSkillsContainer />
