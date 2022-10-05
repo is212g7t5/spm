88526219813d98ae41e951f34e54ae3b3aac05e5
Jonathan <jonathantan1425@gmail.com>
2022-10-05 20:11:50 +0800

    Create get all skills all courses endpoint (untested)

--- backend/app/api/api_v1/endpoints/skill.py
+++ backend/app/api/api_v1/endpoints/skill.py
@@ -1,8 +1,10 @@
 from typing import Any, List
-import json #added
+import json
+
 from fastapi import APIRouter, Body, Depends, HTTPException
 from sqlalchemy.orm import Session
-from sqlalchemy import text #added
+from sqlalchemy import text
+
 from app import crud, schemas
 from app.api import deps
 from app.core.config import settings
@@ -25,6 +27,57 @@ def get_all_skill(
             detail="Skills not found",
         )
     return skills
+
+
+@router.get("/courses")
+def get_all_skills_and_all_courses(
+    db: Session = Depends(deps.get_db),
+) -> Any:
+    """
+    Get all jobs and all their skills.
+    """
+    sql_query = text(
+        """
+        SELECT
+            s.skill_id, s.skill_name, s.skill_desc, s.is_active, sc.courses
+        FROM
+            skill s
+            LEFT JOIN (
+                SELECT
+                    sc.skill_id,
+                    JSON_ARRAYAGG(JSON_OBJECT(
+                        'course_id', c.course_id,
+                        'course_name', c.course_name,
+                        'course_desc', c.course_desc,
+                        'course_status', c.course_status,
+                        'course_type', c.course_type,
+                        'course_category', c.course_category
+                    )) courses
+                FROM skill_course sc, course c
+                WHERE sc.course_id = c.course_id
+                GROUP BY sc.skill_id
+            ) sc ON s.skill_id = sc.skill_id;
+    """
+    )
+    db_cursor_obj = db.execute(sql_query)
+    if not db_cursor_obj:
+        raise HTTPException(
+            status_code=404,
+            detail="Error getting all skills with their courses",
+        )
+
+    skills_with_courses = db_cursor_obj.all()
+    if not skills_with_courses:
+        raise HTTPException(
+            status_code=404,
+            detail="No skills with courses in the database",
+        )
+
+    skills_with_courses = [dict(r) for r in skills_with_courses]
+    for row in skills_with_courses:
+        row["courses"] = json.loads(row["courses"])
+
+    return skills_with_courses
 
 
 @router.get("/{skill_id}", response_model=schemas.Skill)
