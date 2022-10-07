01dcc979962f176cfd19c8c0e7bfca0888e4a12f
Jonathan <jonathantan1425@gmail.com>
2022-10-07 18:49:48 +0800

    Add edit job page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { UserProvider } from "src/contexts/UserContext";
 import { LJCreationContextProvider } from "src/contexts/LJCreationContext";
-import { Courses, Jobs, LearningJourneys, CreateLearningJourneyPage } from "src/routes";
+import { UpdateJobContextProvider } from "src/contexts/UpdateJobContext";
+import { Courses, Jobs, CreateJob, UpdateJob, LearningJourneys, CreateLearningJourneyPage } from "src/routes";
 
 import Layout from "./layout";
 import "./App.css";
@@ -12,30 +13,34 @@ function App() {
   return (
     <Router>
       <LJCreationContextProvider>
-        <UserProvider>
-          <Layout>
-            <Switch>
-              <Route exact path='/' component={LearningJourneys} />
-              <Route exact path='/courses' component={Courses} />
-              <Route exact path='/jobs' component={Jobs} />
-              <Route exact path='/skills' component={Jobs} />
-              <Route exact path='/dashboard' component={Jobs} />
-              <Route path='/create-learning-journey' component={CreateLearningJourneyPage} />
-            </Switch>
-          </Layout>
-          <ToastContainer
-            position='bottom-right'
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss={false}
-            draggable
-            pauseOnHover={false}
-            theme='colored'
-          />
-        </UserProvider>
+        <UpdateJobContextProvider>
+          <UserProvider>
+            <Layout>
+              <Switch>
+                <Route exact path='/' component={LearningJourneys} />
+                <Route exact path='/courses' component={Courses} />
+                <Route exact path='/jobs' component={Jobs} />
+                <Route exact path='/create-job' component={CreateJob} />
+                <Route exact path='/update-job' component={UpdateJob} />
+                <Route exact path='/skills' component={Jobs} />
+                <Route exact path='/dashboard' component={Jobs} />
+                <Route path='/create-learning-journey' component={CreateLearningJourneyPage} />
+              </Switch>
+            </Layout>
+            <ToastContainer
+              position='bottom-right'
+              autoClose={3000}
+              hideProgressBar={false}
+              newestOnTop
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss={false}
+              draggable
+              pauseOnHover={false}
+              theme='colored'
+            />
+          </UserProvider>
+        </UpdateJobContextProvider>
       </LJCreationContextProvider>
     </Router>
   );
--- app/src/routes/index.jsx
+++ app/src/routes/index.jsx
@@ -1,7 +1,8 @@
 import Courses from "./courses";
 import CreateJob from "./createJob";
+import UpdateJob from "./updateJob";
 import Jobs from "./jobs";
 import LearningJourneys from "./learningJourneys";
 import CreateLearningJourneyPage from "./createLearningJourneyPage";
 
-export { Courses, CreateJob, Jobs, LearningJourneys, CreateLearningJourneyPage };
+export { Courses, CreateJob, UpdateJob, Jobs, LearningJourneys, CreateLearningJourneyPage };
