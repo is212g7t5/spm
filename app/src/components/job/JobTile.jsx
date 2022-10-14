b4c3affa86d67dcb6e0a87d56b7a7bef854e46f4
charmjj <charmainelimjj@gmail.com>
2022-10-14 15:41:58 +0800

    add Edit Skill button to skill row, fix color className for Edit Job button

--- app/src/components/job/JobTile.jsx
+++ app/src/components/job/JobTile.jsx
@@ -65,7 +65,7 @@ function CreateEditJobButton({ handleEditJobButtonClick }) {
   return (
     <button
       type='button'
-      className='w-full flex items-center justify-center ml-auto text-textColor bg-secondaryColor hover:bg-tertiaryColor focus:ring-4 rounded-lg text-sm px-5 py-2.5 text-center m-1'
+      className='w-full flex items-center justify-center ml-auto text-textColor bg-secondary hover:bg-tertiary focus:ring-4 rounded-lg text-sm px-5 py-2.5 text-center m-1'
       onClick={handleEditJobButtonClick}
     >
       <PencilSquareIcon className='mr-2 h-5 w-5' aria-hidden='true' />
--- app/src/components/skill/SkillTile.jsx
+++ app/src/components/skill/SkillTile.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
-import { ChevronDownIcon, ChevronRightIcon, StarIcon } from "@heroicons/react/20/solid";
+import { ChevronDownIcon, ChevronRightIcon, StarIcon, PencilSquareIcon } from "@heroicons/react/20/solid";
+import { useUserContext } from "src/contexts/UserContext";
 import CourseBadge from "./CourseBadge";
 
 export default function SkillTile({ skillId, skillName, skillDesc, courses, isActive }) {
   const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+  const { currentUserType } = useUserContext();
+
+  const handleEditSkillButtonClick = () => {
+    console.log("Edit Skill selected");
+  }
 
   return (
     <div className='container flex-col'>
@@ -22,10 +28,26 @@ export default function SkillTile({ skillId, skillName, skillDesc, courses, isAc
             <div className='text-gray-600 text-sm text-left'>{skillId}</div>
           </div>
         </div>
-        <SkillTileButton isDetailsOpen={isDetailsOpen} setIsDetailsOpen={setIsDetailsOpen} />
+        <div className="flex items-center">
+          {currentUserType === "HR" && (<CreateEditSkillButton handleEditSkillButtonClick={handleEditSkillButtonClick} />)}
+          <SkillTileButton isDetailsOpen={isDetailsOpen} setIsDetailsOpen={setIsDetailsOpen} />
+        </div>
       </div>
       {isDetailsOpen && <SkillTileDescription skillDesc={skillDesc} courses={courses} />}
     </div>
+  );
+}
+
+function CreateEditSkillButton({ handleEditSkillButtonClick }) {
+  return (
+    <button
+      type='button'
+      className='w-full flex items-center mr-5 justify-center ml-auto text-textColor bg-secondary hover:bg-tertiary focus:ring-4 rounded-lg text-sm px-5 py-2.5 text-center m-1'
+      onClick={handleEditSkillButtonClick}
+    >
+      <PencilSquareIcon className='mr-2 h-5 w-5' aria-hidden='true' />
+      <span>Edit</span>
+    </button>
   );
 }
 
