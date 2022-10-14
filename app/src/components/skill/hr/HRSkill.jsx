6a7cb5fbcc2dd9dd042b4a010c0962193749c7cb
charmjj <charmainelimjj@gmail.com>
2022-10-14 09:06:26 +0800

    call BE API to get all skills for HRSkill

--- app/src/components/skill/hr/HRSkill.jsx
+++ app/src/components/skill/hr/HRSkill.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { mockSkillAndCourses } from "src/utils/mocks";
+import React, { useEffect, useState } from "react";
+import { getAllSkillsAndCourses } from "src/api/skills";
+
 import SkillTile from "../SkillTile";
 
 function HRSkill() {
-
-  const skills = mockSkillAndCourses;
+  const [skills, setSkills] = useState([]);
 
   const renderSkills = skills.map(({ skillId, skillName, skillDesc, courses, isActive }, index) => (
     <SkillTile
@@ -16,6 +16,15 @@ function HRSkill() {
       isActive={isActive}
     />
   ));
+
+  useEffect(() => {
+    async function getAllSkills() {
+      const skillsReturnedFromBackend = await getAllSkillsAndCourses();
+      setSkills(skillsReturnedFromBackend);
+    }
+
+    getAllSkills();
+  }, []);
 
   return (
     <div className='flex flex-col container w-11/12 max-w-7xl mt-10 bg-white p-10 mx-auto w-full rounded-lg shadow-lg shadow-blue-200'>
