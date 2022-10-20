7593887367d5ee1752a99449605230090cfef0da
branfats <92558018+branfats@users.noreply.github.com>
2022-10-20 14:31:29 +0800

    Call getCourseStatus API to View Course Status

--- app/src/components/course/CourseStatusBadge.jsx
+++ app/src/components/course/CourseStatusBadge.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { getRegistrationByStaffAndCourseId } from "src/api/registration";
 
-export default function CourseStatusBadge({registrationStatus, completionStatus}) {
+export default function CourseStatusBadge({staffId, courseId}) {
   const statusToBadgeClass = {
     "Completed": "bg-green-100 text-green-800",
-    "Rejected": "bg-red-100 text-red-800", 
+    "Rejected": "bg-red-100 text-red-800",
     "Waitlisted": "bg-blue-200 text-blue-800",
     "Registered": "bg-yellow-100 text-yellow-800",
-    "Not Registered": "bg-purple-100 text-purple-800"
+    "Ongoing": "bg-purple-100 text-purple-800",
+    "Not Registered": "bg-gray-100 text-gray-800"
   };
 
-  if (completionStatus === "Completed") {
+  const [courseStatus, setCourseStatus] = useState("");
+
+  useEffect(() => {
+    async function getCourseStatus(staffId, courseId) {
+      const registrationData = await getRegistrationByStaffAndCourseId(staffId, courseId);
+      if (Object.keys(registrationData).length === 0) {
+        setCourseStatus("Not Registered");
+      } else {
+        setCourseStatus(
+          registrationData.completionStatus.length > 0
+            ? registrationData.completionStatus.trim()
+            : registrationData.regStatus.trim(),
+        );
+      }
+    }
+
+    getCourseStatus(staffId,courseId);
+  }, []);
+
+  if (courseStatus === "Completed") {
     return (
-      <span className={`${statusToBadgeClass[completionStatus]} text-sm font-medium mx-3 px-2.5 py-0.5 rounded`}>
-        {completionStatus}
+      <span className={`${statusToBadgeClass[courseStatus]} text-sm font-medium mx-3 px-2.5 py-0.5 rounded`}>
+        {courseStatus}
       </span>
     );
   }
 
   return (
-    <span className={`${statusToBadgeClass[registrationStatus]} text-sm font-medium mx-3 px-2.5 py-0.5 rounded`}>{registrationStatus}</span>
+    <span className={`${statusToBadgeClass[courseStatus]} text-sm font-medium mx-3 px-2.5 py-0.5 rounded`}>{courseStatus}</span>
   );
 }
