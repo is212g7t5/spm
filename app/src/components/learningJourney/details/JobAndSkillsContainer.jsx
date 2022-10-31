da9bd476ae7868a11f9f8e5f44c978d00b31a2d4
charmjj <charmainelimjj@gmail.com>
2022-11-01 00:02:25 +0800

    no active skills message for job role in LJ

--- app/src/components/learningJourney/details/JobAndSkillsContainer.jsx
+++ app/src/components/learningJourney/details/JobAndSkillsContainer.jsx
@@ -6,12 +6,18 @@ export default function JobAndSkillsContainer({ LJId, skills, jobName, isJobActi
     <JobSkillBadge skillName={skill.skillName} key={index} />
   ));
 
+  const noActiveSkillsMessage = <div className="text-sm italic text-center">
+    No active skills for this job role
+  </div>;
+
   return (
     <div className='flex flex-col w-48 mt-1 ml-9 lg:col-span-1 col-span-2'>
       <h2 className='mb-1 lg:text-lg font-bold text-center'>Target Job Role:</h2>
       <JobBadge jobName={jobName} isActive={isJobActive} />
 
-      <div className='flex flex-col mt-2 rounded-lg shadow-lg'>{skillList}</div>
+      <div className='flex flex-col mt-2 rounded-lg shadow-lg'>
+        {skillList.length === 0 ? noActiveSkillsMessage : skillList}
+      </div>
     </div>
   );
 }
