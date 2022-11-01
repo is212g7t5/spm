592f25dd57a08c66956fae11d282ca479328a1b1
jonathantan1425 <jonathantan1425@gmail.com>
2022-11-01 22:55:59 +0800

    Add updateCourseContextProvider

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,6 +4,7 @@ import { UserProvider } from "src/contexts/UserContext";
 import { LJContextProvider } from "src/contexts/LJContext";
 import { UpdateJobContextProvider } from "src/contexts/UpdateJobContext";
 import { UpdateSkillContextProvider } from "src/contexts/UpdateSkillContext";
+import { UpdateCourseContextProvider } from "src/contexts/UpdateCourseContext";
 import {
   Courses,
   Jobs,
@@ -27,35 +28,37 @@ function App() {
       <LJContextProvider>
         <UpdateJobContextProvider>
           <UpdateSkillContextProvider>
-            <UserProvider>
-              <Layout>
-                <Switch>
-                  <Route exact path='/' component={LearningJourneys} />
-                  <Route path='/learning-journeys' component={LearningJourneys} />
-                  <Route exact path='/create-learning-journey' component={CreateLearningJourneyPage} />
-                  <Route exact path='/courses' component={Courses} />
-                  <Route exact path='/update-course' component={UpdateCourse} />
-                  <Route exact path='/skills' component={Skills} />
-                  <Route exact path='/create-skill' component={CreateSkill} />
-                  <Route exact path='/jobs' component={Jobs} />
-                  <Route exact path='/create-job' component={CreateJob} />
-                  <Route exact path='/update-job' component={UpdateJob} />
-                  <Route exact path='/update-skill' component={UpdateSkill} />
-                </Switch>
-              </Layout>
-              <ToastContainer
-                position='bottom-right'
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss={false}
-                draggable
-                pauseOnHover={false}
-                theme='colored'
-              />
-            </UserProvider>
+            <UpdateCourseContextProvider>
+              <UserProvider>
+                <Layout>
+                  <Switch>
+                    <Route exact path='/' component={LearningJourneys} />
+                    <Route path='/learning-journeys' component={LearningJourneys} />
+                    <Route exact path='/create-learning-journey' component={CreateLearningJourneyPage} />
+                    <Route exact path='/courses' component={Courses} />
+                    <Route exact path='/update-course' component={UpdateCourse} />
+                    <Route exact path='/skills' component={Skills} />
+                    <Route exact path='/create-skill' component={CreateSkill} />
+                    <Route exact path='/jobs' component={Jobs} />
+                    <Route exact path='/create-job' component={CreateJob} />
+                    <Route exact path='/update-job' component={UpdateJob} />
+                    <Route exact path='/update-skill' component={UpdateSkill} />
+                  </Switch>
+                </Layout>
+                <ToastContainer
+                  position='bottom-right'
+                  autoClose={3000}
+                  hideProgressBar={false}
+                  newestOnTop
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss={false}
+                  draggable
+                  pauseOnHover={false}
+                  theme='colored'
+                />
+              </UserProvider>
+            </UpdateCourseContextProvider>
           </UpdateSkillContextProvider>
         </UpdateJobContextProvider>
       </LJContextProvider>
