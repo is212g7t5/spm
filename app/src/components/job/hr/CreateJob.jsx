117ab712c6ecb255a34d21e4e988bac75826da63
jonathantan1425 <jonathantan1425@gmail.com>
2022-11-01 19:35:55 +0800

    Add foreach check and fallback

--- app/src/components/job/hr/CreateJob.jsx
+++ app/src/components/job/hr/CreateJob.jsx
@@ -18,9 +18,13 @@ export default function HRCreateJob() {
     // If detail key exists in response, there are schematic errors in the request
     if (res.detail && res.detail.length > 0) {
       const errorList = [];
-      res.detail.forEach((error) => {
-        errorList.push(error.msg);
-      }); 
+      if (res.detail.forEach) {
+        res.detail.forEach((error) => {
+          errorList.push(error.msg);
+        }); 
+      } else {
+        errorList.push(res.detail);
+      }
       setErrors(errorList);
     } else {
       setErrors([]);
