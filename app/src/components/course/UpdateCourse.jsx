ff8600ab66640e9c04851c56c83718c1a77213f4
branfats <92558018+branfats@users.noreply.github.com>
2022-11-02 21:38:23 +0800

    Create Submit Button

--- app/src/components/course/UpdateCourse.jsx
+++ app/src/components/course/UpdateCourse.jsx
@@ -2,11 +2,10 @@ import { useState, useEffect } from "react";
 import { useUpdateCourseContext } from "src/contexts/UpdateCourseContext";
 import { useUserContext } from "src/contexts/UserContext";
 import { getSkillIdsForCourse } from "src/api/skillCourse";
-import { getSkillsObject } from "src/api/skills"
+import { getSkillsObject } from "src/api/skills";
 // import UpdateCourseSuccess from "./UpdateCourseSuccess";
 import { XMarkIcon } from "@heroicons/react/20/solid";
 import CourseDescription from "./CourseDescription";
-
 
 export default function HRUpdateCourse() {
   const { currentUserType } = useUserContext();
@@ -20,18 +19,17 @@ export default function HRUpdateCourse() {
   const [selectedSkills, setSelectedSkills] = useState([]);
   const [defaultSkillValue, setDefaultSkillValue] = useState("default");
 
-
   const handleSkillChange = (e) => {
     e.preventDefault();
     const newSkillId = parseInt(e.target.value, 10);
     if (!selectedSkills.includes(newSkillId)) {
-      setSelectedSkills([...selectedSkills, newSkillId])
+      setSelectedSkills([...selectedSkills, newSkillId]);
     }
-    setDefaultSkillValue("default")
-  }
+    setDefaultSkillValue("default");
+  };
 
   useEffect(() => {
-      getAllSkills();
+    getAllSkills();
 
     async function getAllSkills() {
       const skillsReturnedFromBackend = await getSkillsObject(true);
@@ -41,26 +39,31 @@ export default function HRUpdateCourse() {
     }
   }, []);
 
+  console.log(allActiveSkills);
+  console.log(selectedSkills);
+
   const renderSkillsOptions = Object.keys(allActiveSkills).map((skillId) => (
     <option value={skillId}>{allActiveSkills[skillId].skillName}</option>
-  ))
+  ));
 
-
-  const removeSkill = skillId => () => {
-    setSelectedSkills(selectedSkills => selectedSkills.filter((selectedSkillId) => selectedSkillId !== skillId));
-  }
+  const removeSkill = (skillId) => () => {
+    setSelectedSkills((selectedSkills) =>
+      selectedSkills.filter((selectedSkillId) => selectedSkillId !== skillId),
+    );
+  };
 
   const renderSelectedSkills = selectedSkills.map((skillId) => (
-    <div className="flex bg-primaryColor text-textColor mr-2 px-3 py-1 space-x-2 rounded">
+    <div className='flex bg-primaryColor text-textColor mr-2 px-3 py-1 space-x-2 rounded'>
       <span>{allActiveSkills[skillId].skillName}</span>
-      <button type="button" onClick={removeSkill(skillId)}>
-        <XMarkIcon className="h-6 w-6 text-textColor2"/>
+      <button type='button' onClick={removeSkill(skillId)}>
+        <XMarkIcon className='h-6 w-6 text-textColor2' />
       </button>
     </div>
-  ))
+  ));
 
   const handleSubmit = async (e) => {
     // to be filled in. Use Create Skill Course API
+    
   };
 
   switch (currentUserType) {
@@ -69,25 +72,40 @@ export default function HRUpdateCourse() {
         <div className='relative flex flex-col container max-w-7xl mt-10 bg-white p-10 mx-auto rounded-lg shadow-lg shadow-blue-200'>
           <h1 className='text-3xl text-left font-bold'>Update Course</h1>
           <form onSubmit={handleSubmit}>
-          <p className='font-medium text-xl text-justify'>You have selected Course: {courseName}</p>
-          <div className='flex flex-col w-full p-3 px-5 bg-gray-100 rounded-lg mt-5'>
-            <p className='text-lg font-bold text-black dark:text-white'>Course Description:</p>
-            <CourseDescription courseDesc={courseDesc} skills={skills} />
-          </div>
-          <div className = "mb-6">
-            <label htmlFor="courseDesc" className="block mb-2 text-md font-medium text-gray-900 dark:text-gray-300 space-y-2">
+            <p className='font-medium text-xl text-justify'>
+              You have selected Course: {courseName}
+            </p>
+            <div className='flex flex-col w-full p-3 px-5 bg-gray-100 rounded-lg mt-5'>
+              <p className='text-lg font-bold text-black dark:text-white'>Course Description:</p>
+              <CourseDescription courseDesc={courseDesc} skills={skills} />
+            </div>
+            <div className='mb-6'>
+              <label
+                htmlFor='courseDesc'
+                className='block mb-2 text-md font-medium text-gray-900 dark:text-gray-300 space-y-2'
+              >
                 <p>Skills</p>
-                <p className="italic font-light text-gray-400 text-sm">At least 1 skill</p>
-                <select id="underline_select" onChange={handleSkillChange} className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer" value={defaultSkillValue}>
-                    <option selected value="default" disabled hidden>Assign a skill</option>
-                    {renderSkillsOptions}
+                <p className='italic font-light text-gray-400 text-sm'>At least 1 skill</p>
+                <select
+                  id='underline_select'
+                  onChange={handleSkillChange}
+                  className='block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer'
+                  value={defaultSkillValue}
+                >
+                  <option selected value='default' disabled hidden>
+                    Assign a skill
+                  </option>
+                  {renderSkillsOptions}
                 </select>
-            </label>
-            <div className="flex space-x-2">
-                {renderSelectedSkills}
-              </div>
-          </div>
-          {/* Button */}
+              </label>
+              <div className='flex space-x-2'>{renderSelectedSkills}</div>
+            </div>
+            <button
+              type='submit'
+              className='text-white bg-accent2 hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+            >
+              Assign skills to course
+            </button>
           </form>
         </div>
       );
